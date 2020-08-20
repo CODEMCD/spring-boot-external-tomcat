b7631e4d9477751030946155dd42040fb57369ba
codemcd <psbum1609@gmail.com>
2020-08-20 18:09:01 +0900

    init project

---
+++ src/main/java/com/codemcd/springbootexternaltomcat/SpringBootExternalTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.codemcd.springbootexternaltomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExternalTomcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExternalTomcatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codemcd/springbootexternaltomcat/SpringBootExternalTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemcd.springbootexternaltomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExternalTomcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
